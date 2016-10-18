200240ceae86c8372505f19bf5b2db91658ce17f
Scott Krohn <skrohn86@gmail.com>
2016-10-18 14:30:54 -0700

    Updated connect file

--- model/connect_db.php
+++ model/connect_db.php
@@ -2,7 +2,7 @@
 
 	/****************************************************************************
 		This script connects to a database by creating a PDO object called $db.
-		If connecting to the database fails, it will create an error message 
+		If connecting to the database fails, it will create an error message
 		string based on the exception thrown by the PDO object. It will then
 		include a php page to display the error to the user.
 	*****************************************************************************/
@@ -10,7 +10,7 @@
 	// Set the login credentials for the PDO object
 	$dsn = 'mysql:host=localhost;dbname=cathykrohndesigns';
 	$user = 'root';
-	$pass = 'refinnej8!';
+	$pass = '';
 
 	// Attempt to login to the datbase, display error if failed
 	try {
